7e9142effa14d6a82fcf6e208a8467a73d614973
Deep Kumar Sherathiya <deep.sherathiya@adoptnet.tech>
2023-02-01 17:12:44 +0530

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,56 @@
+import { Component, ViewChild } from "@angular/core";
+import { Customer, Representative } from "./customer";
+import { CustomerService } from "./customerservice";
+import { MessageService } from "primeng/api";
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  providers: [MessageService]
+})
+export class AppComponent {
+  customers: Customer[];
+
+  representatives: Representative[];
+
+  statuses: any[];
+
+  loading: boolean = true;
+
+  activityValues: number[] = [0, 100];
+
+  constructor(private customerService: CustomerService) {}
+
+  ngOnInit() {
+    this.customerService.getCustomersLarge().then(customers => {
+      this.customers = customers;
+      this.loading = false;
+
+      this.customers.forEach(
+        customer => (customer.date = new Date(customer.date))
+      );
+    });
+
+    this.representatives = [
+      { name: "Amy Elsner", image: "amyelsner.png" },
+      { name: "Anna Fali", image: "annafali.png" },
+      { name: "Asiya Javayant", image: "asiyajavayant.png" },
+      { name: "Bernardo Dominic", image: "bernardodominic.png" },
+      { name: "Elwin Sharvill", image: "elwinsharvill.png" },
+      { name: "Ioni Bowcher", image: "ionibowcher.png" },
+      { name: "Ivan Magalhaes", image: "ivanmagalhaes.png" },
+      { name: "Onyama Limba", image: "onyamalimba.png" },
+      { name: "Stephen Shaw", image: "stephenshaw.png" },
+      { name: "XuXue Feng", image: "xuxuefeng.png" }
+    ];
+
+    this.statuses = [
+      { label: "Unqualified", value: "unqualified" },
+      { label: "Qualified", value: "qualified" },
+      { label: "New", value: "new" },
+      { label: "Negotiation", value: "negotiation" },
+      { label: "Renewal", value: "renewal" },
+      { label: "Proposal", value: "proposal" }
+    ];
+  }
+}
